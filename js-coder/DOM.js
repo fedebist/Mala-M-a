50ff99dcc892562f6a0276792b1156c64e4bf82b
fedebist <fedebistolfi9@gmail.com>
2021-11-22 17:55:21 -0300

    DOMM

---
+++ js-coder/DOM.js
@@ -0,0 +1,14 @@
+function añadir(){
+    let nombre = document.getElementById('nombreProducto');
+    let añadido = document.getElementById('añadido');
+
+    let confirmacion = document.createElement('p');
+    confirmacion.innerHTML = `<p>Nombre: ${nombre.value}</p>
+    <h5>Se añadió al carrito</h5>`;
+    añadido.appendChild(confirmacion);
+}
+
+function borrar(){
+    let añadido = document.getElementById('añadido');
+    añadido.removeChild(añadido.lastChild);
+}
