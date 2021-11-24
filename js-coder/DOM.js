a7a7f6fba13afce75b6887619ce2d63fc59c0697
fedebist <fedebistolfi9@gmail.com>
2021-11-24 20:10:34 -0300

    dom

--- js-coder/DOM.js
+++ js-coder/DOM.js
@@ -1,5 +1,5 @@
 function añadir(){
-    let nombre = document.getElementsByClassName('nombreProducto');
+    let nombre = document.getElementsByClassName('h5.fw-bolder.nombreProducto');
     let añadido = document.getElementById('añadido');
     console.log(nombre);
 
