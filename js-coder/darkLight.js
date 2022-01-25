6f6e78d0e9a33d15c7c0e7e798552f934e7fabc0
fedebist <fedebistolfi9@gmail.com>
2022-01-25 20:43:51 -0300

    darkLight

--- js-coder/darkLight.js
+++ js-coder/darkLight.js
@@ -7,13 +7,17 @@ function darkMode(){
     /* Fondo carrito */
     $('#carritoCompras').css('background-color', 'grey');
     
-    let navBar = document.getElementsByTagName('nav');
-    navBar.style.background = 'black';
-
     $('.añadirCarrito').css('background-color', 'grey');
 
     $('#productos').css('background-color', 'black');
   
+    /* filtros */
+    $('.categorias').css('color', 'white');
+    
+    /* titulo filtrado */
+    $('.tituloFiltrar').css('color', 'white');
+
+    $('.tituloProductos').css('color', 'white');
   }
   
   function lightMode(){
@@ -24,5 +28,15 @@ function darkMode(){
      
     /* Fondo carrito */
     $('#carritoCompras').css('background-color', 'white');
+
+    $('.categorias').css('color', 'black');
+    
+    /* titulo de filtrado */
+    $('.tituloFiltrar').css('color', 'black');
+     
+    /* lista productos index */
+    $('#productos').css('background-color', 'white');
+
+    $('.tituloProductos').css('color', 'black');
   }
 
